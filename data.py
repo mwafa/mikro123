3be8c22aa853044322a4551d98500a5f9072b7a6
drfauziah <50840553+drfauziah@users.noreply.github.com>
2019-05-24 12:24:33 +0700

    Update data.py

--- data.py
+++ data.py
@@ -12,9 +12,27 @@ data = {
 	"dimana": ["dimana", "di mana", "di manakah"],
 	"kenapa": ["kenapa", "mengapa"],
 	"siapa": ["siapa", "siapakah"],
+        "berapa": ["berapa", "pinten", "piro"],
 
 	"laki-laki": ["laki-laki", "pria", "cowok"],
 	"pacar": ["pacar"],
 	"perempuan": ["perempuan", "cewek", "wanita"],
-	"teman": ["teman", "temen"]
-}
+	"teman": ["teman", "temen"],
+        
+        "makan": ["makan", "nedho", "dhahar", "maem"],
+        "minum": ["minum"],
+        "belajar": ["belajar", "sinau", "maos"],
+        "pergi": ["pergi", "berangkat", "lungo"],
+
+        "halo": ["halo", "hai", "hallo"],
+        "kabar": ["kabar"],
+        "baik": ["baik", "bagus"],
+        "terimakasih": ["terima kasih", "terimakasih", "makasih", "suwun", "nuwun", "mantur suwun", "mantur nuwun"],
+        "sama sama": ["samasama", "sama sama", "sami sami", "podo podo"],
+        "suka": ["suka", "seneng"],
+
+        "sekolah": ["sekolah", "kampus"],
+        "kelas": ["kelas"],
+        "rumah": ["rumah", "omah"],
+ 
+       }
